66247012853731d8c7d286a40d64615b120aada8
Marcelo Ribeiro@LAPTOP-Lenovo.Marcelo <mrcomputerinfo@gmail.com>
2021-07-05 08:23:25 -0300

    funcao de numero de parcelas

--- src/calcula-valor.js
+++ src/calcula-valor.js
@@ -12,7 +12,14 @@ function arredondar (valor) {
   return arredondado
 }
 
+function calcularPrestacoes (montante, numeroParcelas) {
+  const prestacaoBase = arredondar(montante / numeroParcelas)
+  const resultado = Array(numeroParcelas).fill(prestacaoBase)
+  return resultado
+}
+
 module.exports = {
   calcularMontante,
-  arredondar
+  arredondar,
+  calcularPrestacoes
 }
